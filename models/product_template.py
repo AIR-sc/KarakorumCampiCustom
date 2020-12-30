1f2e5fa271d13126643d8c7ae654fc04349d31f8
Simone C <simone.cuffaro@air-srl.com>
2020-12-30 15:07:44 +0100

    Aggiunti campi pubblicazione web

--- models/product_template.py
+++ models/product_template.py
@@ -8,6 +8,9 @@ class product_template(models.Model):
 	_inherit = "product.template"
 
 	# CARATTERISTICHE
-	collezione = fields.Many2one('karakorum.collezione', string="Collezione")
-	stagione = fields.Many2one('karakorum.stagione', string="Stagione")
-	confezione_associata = fields.Char(string="Conf. associata")
+	collezione = fields.Many2one('karakorum.collezione', string="Collezione", help="Associa il prodotto ad una collezione.")
+	stagione = fields.Many2one('karakorum.stagione', string="Stagione", help="Associa il prodotto ad una stagione.")
+	confezione_associata = fields.Char(string="Conf. associata", help="Specifica il numero delle confezioni omaggio associate.")
+
+	pubblica_web_rivenditori = fields.Boolean(string="Pubblica sito riv.", help="Spuntare la casella per pubblicare l'articolo sul sito dei rivenditori (.tv)")
+	pubblica_web_b2c = fields.Boolean(string="Pubblica sito B2C", help="Spuntare la casella per pubblicare l'articolo sul sito B2C (.it)")
